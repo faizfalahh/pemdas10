973fddf0c372186f3040c59d6d69c5647379f06f
faizfalahh <faizfalah38@gmail.com>
2026-05-04 17:51:45 +0800

    menambahkan overloading constructor dan method tampilkan pada class Produk

--- tugas.cpp
+++ tugas.cpp
@@ -15,3 +15,45 @@ private:
         nama_Produk = "tidak ada";
     }
 
+ Produk(int id)
+    {
+        id_Produk = id;
+        nama_Produk = "tidak ada";
+    }
+
+    Produk(string nama)
+    {
+        nama_Produk = nama;
+        id_Produk = 0;
+    }
+
+    Produk(int id, string nama)
+    {
+        id_Produk = id;
+        nama_Produk = nama;
+    }
+
+    void tampilkan()
+    {
+        cout << "Id Produk : " << id_Produk << endl;
+        cout << "Nama Produk : " << nama_Produk << endl;
+    }
+};
+
+int main()
+{
+
+    cout << "=== Daftar Inventaris Toko ===" << endl;
+
+    Produk p1;
+    p1.tampilkan();
+
+    Produk p2(11);
+    p2.tampilkan();
+
+    Produk p3("Laptop");
+    p3.tampilkan();
+
+    Produk p4(21, "Keyboard");
+    p4.tampilkan();
+}
