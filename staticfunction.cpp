a02ced8a05eff922aaedd7d325f1d99af1d9a41f
faizfalahh <faizfalah38@gmail.com>
2026-05-04 17:09:47 +0800

    implementasi variabel static dan method pada class Mahasiswa

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -28,5 +28,19 @@ public:
     }
 };
 
+int Mahasiswa::nim = 0;
+
+//pendefinisian prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+
 
    
