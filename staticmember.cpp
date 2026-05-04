a9a260feff4867d15f531a0799977a5a85785497
faizfalahh <faizfalah38@gmail.com>
2026-05-04 16:26:38 +0800

    membuat class mahasiswa, menambahkan variabel static nim, menambahkan atribut id dan nama sebagai member variabel

---
+++ staticmember.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa { //membuat class mahasiswa
+    
+public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
+    }
